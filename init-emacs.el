3610e1df5fc218a7ed9471f979984bcd814f486e
apple115 <2567031030@qq.com>
2024-05-07 19:38:48 +0800

    添加 init-emacs 的简单测试一下包

---
+++ init-emacs.el
@@ -0,0 +1,16 @@
+;; init-emacs.el
+;; emacs -Q -l init-emacs.el
+
+(setq package-enable-at-startup nil)
+(setq package-archives
+      '(("melpa-cn" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
+        ("gnu-cn"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+        ("nongnu-cn"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/")))
+
+(package-initialize)
+(use-package vterm
+  :ensure t
+  )
+
+
+
