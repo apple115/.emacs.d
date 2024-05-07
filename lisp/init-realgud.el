562fa6a178d1cf56858d05c09c504198921fb4b6
apple115 <2567031030@qq.com>
2024-05-07 19:37:24 +0800

    添加这个realgud

---
+++ lisp/init-realgud.el
@@ -0,0 +1,11 @@
+;;; init-realgud.el --- Langs settings -*- lexical-binding: t -*-
+;;; Commentary: realgud
+
+;;; Code:
+(use-package realgud
+ :ensure t)
+
+(provide 'init-realgud)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; init-realgud.el ends here
