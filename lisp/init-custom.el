c0a63bb2ec04bd7685946692aac0fe7b0ec0c87e
apple115 <2567031030@qq.com>
2025-06-08 16:25:30 +0800

    feat: 添加自定义的el文件

---
+++ lisp/init-custom.el
@@ -0,0 +1,23 @@
+;;; init-custom.el --- custom settings. -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; my personal custom settings
+;;; Code:
+
+(defcustom my-org-directory (expand-file-name "~/Documents/org")
+  "Set org directory."
+  :group 'my
+  :type 'string)
+
+(defcustom my-proxy "127.0.0.1:7897"
+  "Set HTTP/HTTPS proxy."
+  :group 'my
+  :type 'string)
+
+(defcustom my-socks-proxy "127.0.0.1:7897"
+  "Set SOCKS proxy."
+  :group 'my
+  :type 'string)
+
+(provide 'init-custom)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; init-custom.el ends here
