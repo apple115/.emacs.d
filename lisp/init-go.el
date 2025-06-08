8bc7784eadcf4f7c6bebc2f28fda9b4ab029338a
apple115 <2567031030@qq.com>
2025-06-08 16:24:17 +0800

    feat: 优化go的模块

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -2,28 +2,82 @@
 ;;; Commentary:
 ;;; Code:
 
+(use-package go-mode
+  :ensure t
+  :functions (go-install-tools exec-path-from-shell-copy-envs)
+  :init
+  ;; Install tools
+  (defconst go--tools
+    '("golang.org/x/tools/gopls"
+      "golang.org/x/tools/cmd/goimports"
+      "honnef.co/go/tools/cmd/staticcheck"
+      "github.com/go-delve/delve/cmd/dlv"
+      "github.com/zmb3/gogetdoc"
+      "github.com/josharian/impl"
+      "github.com/cweill/gotests/..."
+      "github.com/fatih/gomodifytags"
+      "github.com/davidrjenni/reftools/cmd/fillstruct")
+    "All necessary go tools.")
+
+  (defun go-install-tools ()
+    "Install or update go tools."
+    (interactive)
+    (unless (executable-find "go")
+      (user-error "Unable to find `go' in `exec-path'!"))
+
+    (message "Installing go tools...")
+    (dolist (pkg go--tools)
+      (set-process-sentinel
+       (start-process "go-tools" "*Go Tools*" "go" "install" "-v" "-x" (concat pkg "@latest"))
+       (lambda (proc _)
+         (let ((status (process-exit-status proc)))
+           (if (= 0 status)
+               (message "Installed %s" pkg)
+             (message "Failed to install %s: %d" pkg status)))))))
+  :config
+  ;; Env vars
+  (with-eval-after-load 'exec-path-from-shell
+    (exec-path-from-shell-copy-envs '("GOPATH" "GO111MODULE" "GOPROXY")))
+  ;; Try to install go tools if `gopls' is not found
+  (when (and (executable-find "go")
+             (not (executable-find "gopls")))
+    (go-install-tools))
+  )
+
+(use-package go-dlv
+  :ensure t)
+(use-package go-fill-struct
+  :ensure t)
+(use-package go-impl
+  :ensure t)
+
+(use-package go-tag
+  :ensure t
+  :bind (:map go-mode-map
+              ("C-c t a" . go-tag-add)
+              ("C-c t r" . go-tag-remove))
+  :init (setq go-tag-args (list "-transform" "camelcase")))
+
+(use-package go-gen-test
+  :ensure t
+  :bind (:map go-mode-map
+              ("C-c t g" . go-gen-test-dwim)))
+
 (use-package go-ts-mode
-:ensure nil
-:mode "\\.go\\'"
-:config
-(setq go-ts-mode-indent-offset 4)
-)
+  :ensure nil
+  :mode "\\.go\\'"
+  :config
+  (setq go-ts-mode-indent-offset 4))
 
 (use-package gotest
   :ensure t
-  :general
-  (:keymaps 'go-ts-mode-map
-  :states '(normal)
-  :prefix  "SPC"
-  "m" '(nil :wk "Go mode")
-  "m t"'(nil :wk "Go mode test")
-  "m t t" '(go-test-current-test :wk "test current")
-  "m t f" '(go-test-current-file :wk "test file")
-  "m t p" '(go-test-current-file :wk "test project")
-  "m t c" '(go-test-current-coverage :wk "test coverage")
-  "m t b" '(go-test-current-benchmark :wk "test benchmark")
-  ))
-
+  :bind (:map go-mode-map
+              ("C-c t f" . go-test-current-file)
+              ("C-c t t" . go-test-current-test)
+              ("C-c t j" . go-test-current-project)
+              ("C-c t b" . go-test-current-benchmark)
+              ("C-c t c" . go-test-current-coverage)
+              ("C-c t x" . go-run)))
 
 (provide 'init-go)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
